f487e7cf403d8074dab99b075546258e19e49996
SkrebovaOlena <e.skrebova21@gmail.com>
2022-07-31 23:11:10 +0300

    5

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,26 @@
 package com.company;
 
+import java.util.Random;
+import java.util.Collections;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        Random random = new Random();
+	    MyList<Integer> myList = new MyList<Integer>();
+
+        for (int i = 0; i < 10; i++) {
+            int number = random.nextInt(100);
+            myList.add(number);
+            System.out.println(number);
+        }
+
+        // short variant
+        System.out.println("Max value (short way) = " + Collections.max(myList.arrayList));
+        System.out.println("Min value (short way) = " + Collections.min(myList.arrayList));
+
+        // long variant
+        System.out.println(myList.toString());
     }
 }
--- src/com/company/MyList.java
+++ src/com/company/MyList.java
@@ -1,2 +1,28 @@
-package com.company;public class MyList {
+package com.company;
+
+import java.util.ArrayList;
+
+public class MyList<T extends Number>  {
+
+    public ArrayList<T> arrayList = new ArrayList<T>();
+
+    public void add(T t) {
+        arrayList.add(t);
+    }
+
+    @Override
+    public String toString() {
+        Integer minValue = (Integer) arrayList.get(0);
+        Integer maxValue = (Integer) arrayList.get(0);
+
+        for (int i = 0; i < arrayList.size(); i++) {
+            Integer value = (Integer)arrayList.get(i);
+            minValue = Math.min(value, minValue);
+            maxValue = Math.max(value, maxValue);
+            if (maxValue < value) {
+                maxValue = value;
+            }
+        }
+        return "Long way: Max value = " + maxValue + ", Min value = " + minValue;
+    }
 }
