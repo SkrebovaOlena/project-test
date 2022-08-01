721eb02630307adb84c98c6455798edca426d4f0
SkrebovaOlena <e.skrebova21@gmail.com>
2022-08-01 13:04:09 +0300

    hw5

--- src/com/company/MyList.java
+++ src/com/company/MyList.java
@@ -10,19 +10,26 @@ public class MyList<T extends Number>  {
         arrayList.add(t);
     }
 
-    @Override
-    public String toString() {
+    public Number getMinValue(){
         Integer minValue = (Integer) arrayList.get(0);
-        Integer maxValue = (Integer) arrayList.get(0);
-
         for (int i = 0; i < arrayList.size(); i++) {
             Integer value = (Integer)arrayList.get(i);
             minValue = Math.min(value, minValue);
-            maxValue = Math.max(value, maxValue);
-            if (maxValue < value) {
-                maxValue = value;
-            }
         }
-        return "Long way: Max value = " + maxValue + ", Min value = " + minValue;
+        return minValue;
+    }
+
+    public Number getMaxValue(){
+        Integer maxValue = (Integer) arrayList.get(0);
+        for (int i = 0; i < arrayList.size(); i++) {
+            Integer value = (Integer)arrayList.get(i);
+            maxValue = Math.max(value, maxValue);
+            }
+        return maxValue;
+    }
+
+    @Override
+    public String toString() {
+        return "Long way: Max value = " + getMaxValue() + ", Min value = " + getMinValue();
     }
 }
